19b82db07af971563590abf2e2f9350a3453a497
NguyenPatrick <ultimpact@gmail.com>
2019-02-09 22:13:54 -0500

    Hitbox changes

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var paddle = Object(); //Position based on top left corner
 paddle.height = 65;
 paddle.width = 600;
 paddle.x = (canvas.width - paddle.width) / 2;
-ball.y = canvas.height - paddle.height-ball.radius;
+ball.y = canvas.height - paddle.height - ball.radius;
 var rightPressed = false;
 var leftPressed = false;
 var BRICKROWCOUNT = 26;
@@ -31,16 +31,28 @@ var searched = false;
 var body = document.body;
 var html = document.documentElement;
 
-canvas.height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-canvas.width = Math.max(body.scrollWidth, body.offsetWidth, html.clientWidth, html.scrollWidth, html.offsetWidth);
+canvas.height = Math.max(
+  body.scrollHeight,
+  body.offsetHeight,
+  html.clientHeight,
+  html.scrollHeight,
+  html.offsetHeight
+);
+canvas.width = Math.max(
+  body.scrollWidth,
+  body.offsetWidth,
+  html.clientWidth,
+  html.scrollWidth,
+  html.offsetWidth
+);
 
 //Initialize bricks
 var bricks = [];
 for (var c = 0; c < BRICKCOLUMNCOUNT; c++) {
-    bricks[c] = [];
-    for (var r = 0; r < BRICKROWCOUNT; r++) {
-        bricks[c][r] = { x: 0, y: 0, status: 1 , char: String.fromCharCode(r+97)};
-    }
+  bricks[c] = [];
+  for (var r = 0; r < BRICKROWCOUNT; r++) {
+    bricks[c][r] = { x: 0, y: 0, status: 1, char: String.fromCharCode(r + 97) };
+  }
 }
 
 //Add controls
@@ -49,147 +61,158 @@ document.addEventListener("keyup", keyUpHandler, false);
 document.addEventListener("mousemove", mouseMoveHandler, false);
 document.addEventListener("click", mouseClickHandler, false);
 function keyDownHandler(e) {
-    if (e.key == "Right" || e.key == "ArrowRight") {
-        rightPressed = true;
-    }
-    else if (e.key == "Left" || e.key == "ArrowLeft") {
-        leftPressed = true;
-    }
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = true;
+  } else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = true;
+  }
 }
 
 function keyUpHandler(e) {
-    if (e.key == "Right" || e.key == "ArrowRight") {
-        rightPressed = false;
-    }
-    else if (e.key == "Left" || e.key == "ArrowLeft") {
-        leftPressed = false;
-    }
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = false;
+  } else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = false;
+  }
 }
 
 function mouseMoveHandler(e) {
-    var relativeX = e.clientX - canvas.offsetLeft;
-    if (relativeX > 0 && relativeX < canvas.width) {
-        paddle.x = relativeX - paddle.width / 2;
-    }
+  var relativeX = e.clientX - canvas.offsetLeft;
+  if (relativeX > 0 && relativeX < canvas.width) {
+    paddle.x = relativeX - paddle.width / 2;
+  }
 }
 
-function mouseClickHandler(e){
-    if (ball.x > paddle.x && ball.x < paddle.x + paddle.width) {
-        var xDiff = ball.x-(paddle.x+paddle.width/2); //Difference between ball center and paddle center
-        //Scale the ball distance away from the paddle center to an angle between 0 and 89
-        //NewValue = (((OldValue - OldMin) * (NewMax - NewMin)) / (OldMax - OldMin)) + NewMin
-        var angle = ((xDiff) * (89)) / ((paddle.width / 2));
-        ball.dx = Math.sin(angle*Math.PI/180)*ball.speed;
-        ball.dy = -(Math.cos(angle*Math.PI/180)*ball.speed);
-        document.removeEventListener("click", mouseClickHandler, false);
-    }
+function mouseClickHandler(e) {
+  if (ball.x > paddle.x && ball.x < paddle.x + paddle.width) {
+    var xDiff = ball.x - (paddle.x + paddle.width / 2); //Difference between ball center and paddle center
+    //Scale the ball distance away from the paddle center to an angle between 0 and 89
+    //NewValue = (((OldValue - OldMin) * (NewMax - NewMin)) / (OldMax - OldMin)) + NewMin
+    var angle = (xDiff * 89) / (paddle.width / 2);
+    ball.dx = Math.sin((angle * Math.PI) / 180) * ball.speed;
+    ball.dy = -(Math.cos((angle * Math.PI) / 180) * ball.speed);
+    document.removeEventListener("click", mouseClickHandler, false);
+  }
 }
 //Adding collision for bricks
 function collisionDetection() {
-    for (var c = 0; c < BRICKCOLUMNCOUNT; c++) {
-        for (var r = 0; r < BRICKROWCOUNT; r++) {
-            var brick = bricks[c][r];
-            if (brick.status == 1) {
-                if (ball.x > brick.x && ball.x < brick.x + BRICKWIDTH && ball.y > brick.y && ball.y < brick.y + BRICKHEIGHT) {
-                    ball.dy = -ball.dy;
-                    brick.status = 0;
-                    searchQuery+= brick.char;
-                }
-            }
+  for (var c = 0; c < BRICKCOLUMNCOUNT; c++) {
+    for (var r = 0; r < BRICKROWCOUNT; r++) {
+      var brick = bricks[c][r];
+      if (brick.status == 1) {
+        if (
+          ball.x > brick.x &&
+          ball.x - ball.radius < brick.x + BRICKWIDTH &&
+          ball.y + ball.radius > brick.y &&
+          ball.y - ball.radius < brick.y + BRICKHEIGHT
+        ) {
+          ball.dy = -ball.dy;
+          brick.status = 0;
+          searchQuery += brick.char;
         }
+      }
     }
+  }
 }
 
 function makeImg() {
-    for (var i = 1; i < 27; i++) {
-        var img2 = new Image();
-        var img_path = "images/brick";
-        img_path = img_path.concat(parseInt(i));
-        img_path = img_path.concat(".jpg");
-        img2.src = img_path;
-        brickimg.push(img2);
-    }
+  for (var i = 1; i < 27; i++) {
+    var img2 = new Image();
+    var img_path = "images/brick";
+    img_path = img_path.concat(parseInt(i));
+    img_path = img_path.concat(".jpg");
+    img2.src = img_path;
+    brickimg.push(img2);
+  }
 }
 
 //Draw all the objects
 function drawBall() {
-    ctx.beginPath();
-    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
-    ctx.fillStyle = "#000000";
-    ctx.fill();
-    ctx.closePath();
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+  ctx.fillStyle = "#000000";
+  ctx.fill();
+  ctx.closePath();
 }
 function drawPaddle() {
-    ctx.beginPath();
-    ctx.drawImage(img, paddle.x, canvas.height - paddle.height, paddle.width, paddle.height);
-    ctx.font = "30px Arial";
-    // Just using arbitrary numbers for now
-    ctx.fillText(searchQuery, paddle.x + 15, canvas.height - (paddle.height / 2) + 8);
-    ctx.fill();
-    ctx.closePath();
+  ctx.beginPath();
+  ctx.drawImage(
+    img,
+    paddle.x,
+    canvas.height - paddle.height,
+    paddle.width,
+    paddle.height
+  );
+  ctx.font = "30px Arial";
+  // Just using arbitrary numbers for now
+  ctx.fillText(
+    searchQuery,
+    paddle.x + 15,
+    canvas.height - paddle.height / 2 + 8
+  );
+  ctx.fill();
+  ctx.closePath();
 }
 function drawBricks() {
-    for (var c = 0; c < BRICKCOLUMNCOUNT; c++) {
-        for (var r = 0; r < BRICKROWCOUNT; r++) {
-            if (bricks[c][r].status == 1) {
-                var brickX = (r * (BRICKWIDTH + BRICKPADDING)) + BRICKOFFSETLEFT;
-                var brickY = (c * (BRICKHEIGHT + BRICKPADDING)) + BRICKOFFSETTOP;
-                bricks[c][r].x = brickX;
-                bricks[c][r].y = brickY;
-                ctx.beginPath();
-                ctx.drawImage(brickimg[r], brickX, brickY, BRICKWIDTH, BRICKHEIGHT);
-                ctx.fill();
-                ctx.closePath();
-            }
-        }
+  for (var c = 0; c < BRICKCOLUMNCOUNT; c++) {
+    for (var r = 0; r < BRICKROWCOUNT; r++) {
+      if (bricks[c][r].status == 1) {
+        var brickX = r * (BRICKWIDTH + BRICKPADDING) + BRICKOFFSETLEFT;
+        var brickY = c * (BRICKHEIGHT + BRICKPADDING) + BRICKOFFSETTOP;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.drawImage(brickimg[r], brickX, brickY, BRICKWIDTH, BRICKHEIGHT);
+        ctx.fill();
+        ctx.closePath();
+      }
     }
+  }
 }
 
 //Main loop
 function draw() {
-    //Refresh and draw all canvas objects
-    console.log(document);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBricks();
-    drawBall();
-    drawPaddle();
-    collisionDetection();
-    ball.x += ball.dx;
-    ball.y += ball.dy;
+  //Refresh and draw all canvas objects
+  console.log(document);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBricks();
+  drawBall();
+  drawPaddle();
+  collisionDetection();
+  ball.x += ball.dx;
+  ball.y += ball.dy;
 
-    //Check if ball has hit the left or right edge of the screen
-    if (ball.x > canvas.width - ball.radius || ball.x < ball.radius) {
-        ball.dx = -ball.dx;
-    }
+  //Check if ball has hit the left or right edge of the screen
+  if (ball.x > canvas.width - ball.radius || ball.x < ball.radius) {
+    ball.dx = -ball.dx;
+  }
 
-    //Check if ball has hit the top edge of the screen
-    if (ball.y < ball.radius) {
-        ball.dy = -ball.dy;
+  //Check if ball has hit the top edge of the screen
+  if (ball.y < ball.radius) {
+    ball.dy = -ball.dy;
+  } else if (ball.y > canvas.height - ball.radius) {
+    //Check if ball hits the bottom of the screen
+    if (ball.x > paddle.x && ball.x < paddle.x + paddle.width) {
+      var xDiff = ball.x - (paddle.x + paddle.width / 2); //Difference between ball center and paddle center
+      //Scale the ball distance away from the paddle center to an angle between 0 and 89
+      //NewValue = (((OldValue - OldMin) * (NewMax - NewMin)) / (OldMax - OldMin)) + NewMin
+      var angle = (xDiff * 89) / (paddle.width / 2);
+      ball.dx = Math.sin((angle * Math.PI) / 180) * ball.speed;
+      ball.dy = -(Math.cos((angle * Math.PI) / 180) * ball.speed);
+    } else {
+      window.open("https://www.google.com/search?q=" + searchQuery, "_self");
+      searched = true;
     }
-    else if (ball.y > canvas.height - ball.radius) {//Check if ball hits the bottom of the screen
-        if (ball.x > paddle.x && ball.x < paddle.x + paddle.width) {
-            var xDiff = ball.x-(paddle.x+paddle.width/2); //Difference between ball center and paddle center
-            //Scale the ball distance away from the paddle center to an angle between 0 and 89
-            //NewValue = (((OldValue - OldMin) * (NewMax - NewMin)) / (OldMax - OldMin)) + NewMin
-            var angle = ((xDiff) * (89)) / ((paddle.width / 2));
-            ball.dx = Math.sin(angle*Math.PI/180)*ball.speed;
-            ball.dy = -(Math.cos(angle*Math.PI/180)*ball.speed);
-        }
-        else {
-            window.open("https://www.google.com/search?q=" + searchQuery, "_self");
-            searched = true;
-        }
-    }
+  }
 
-    if (rightPressed && paddle.x < canvas.width - paddle.width) {
-        paddle.x += 7;
-    }
-    else if (leftPressed && paddle.x > 0) {
-        paddle.x -= 7;
-    }
-    if (!searched) {
-        requestAnimationFrame(draw);
-    }
+  if (rightPressed && paddle.x < canvas.width - paddle.width) {
+    paddle.x += 7;
+  } else if (leftPressed && paddle.x > 0) {
+    paddle.x -= 7;
+  }
+  if (!searched) {
+    requestAnimationFrame(draw);
+  }
 }
 makeImg();
 draw();
